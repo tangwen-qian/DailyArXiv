896eb16b547de92b9127b6b72872e723280ad156
qiantw <49673831@qq.com>
2024-04-09 09:15:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ with open("README.md", "r") as f:
     if last_update_date == current_date:
         sys.exit("Already updated today!")
 
-keywords = ["Time Series", "Trajectory", "Graph Neural Networks"] # TODO add more keywords
+keywords = ["Spati", "Temporal", "Time", "Trajector", "Large", "Foundation"] # TODO add more keywords
 
 max_result = 100 # maximum query results from arXiv API for each keyword
 issues_result = 15 # maximum papers to be included in the issue
